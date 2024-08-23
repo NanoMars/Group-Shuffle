bc4a4506dec50b0af048a420de7697c4aab4656b
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-23 13:19:41 +1000

    generating input fields (not entirely working)

--- script.js
+++ script.js
@@ -1,22 +1,52 @@
-const input1 = document.getElementById("input1");
+DivID = 0
+InputID = 0
 
+
+elementArray = []; // every single element will have a unique randomly generated ID used as the div ID and 
+
+function generateDivID() {
+    DivID += 1;
+    return "div" + DivID.toString();
+}
+function generateInputID() {
+    InputID += 1;
+    return "input" + InputID.toString();
+}
+
+function addInput() {
+    form = document.getElementById("form");
+    thisDiv = document.createElement('div');
+    thisDiv.id = generateDivID();
+    form.append(thisDiv);
+    inputElement = document.createElement('input');
+    inputElement.id = generateInputID();
+    inputElement.type = "text";
+    thisDiv.append(inputElement);
+    thisDiv.addEventListener('input', storeInput);
+    elementArray.push(thisDiv.id);
+}
+
+addInput()
 
 function storeInput(e) {
-    const elementArray = []; // every single element will have a unique randomly generated ID used as the div ID
     
-    const value1 = input1
-    
-    inputArray.push(value1);
     
     console.log(e.target.id);
-
-    if (e.target.value !== "") {
-        elementArray.push(e.target.id);
-    } else if (e.target.value === "" && elementArray[/* self position + 1 */]) {
-        // remove this specific element from the array
+    console.log(elementArray.findIndex(arr => arr.includes(e.target.id)))
+    if (e.target.value === "" && !elementArray[elementArray.findIndex(arr => arr.includes(e.target.id)) + 1]) {
+        addInput();
     }
 
+    
 
+
+
+    /*
+    if (e.target.value !== "") {
+        elementArray.push(e.target.id);
+    } else if (e.target.value === "" && elementArray[/* self position + 1 ]) {
+        // remove this specific element from the array
+    }*/
 }
 
 input1.addEventListener('input', storeInput);
