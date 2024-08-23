6aaf97eff243b9e9f0c68238903dd1742a60aba9
NanoMars <174164929+NanoMars@users.noreply.github.com>
2024-08-23 18:01:46 +1000

    lists populating from the session data, randomised order and displayed on the page.

--- display.js
+++ display.js
@@ -1,7 +1,26 @@
 const groupData = JSON.parse(sessionStorage.getItem('groupData'));
 
+function createList(id) {
+    let list = document.createElement('ul');
+    list.id = id.toString();
+    list.classList.add('inside-container');
+    document.getElementById('outside-container').append(list);
+}
+
+console.log(groupData[1]);
+
 if (groupData) {
-    for (let i of groupData[1]) {
-        console.log(groupData[0][i]);
+    for (let i = 0; i < groupData[1]; i++) {
+        createList(i);
+    }
+
+    names = groupData[0];
+    names.sort(() => Math.random() - 0.5);
+
+    for (i in groupData[0]) {
+        list = document.getElementById(i % groupData[1]);
+        let listItem = document.createElement('li');
+        listItem.textContent = groupData[0][i];
+        list.append(listItem);
     }
 }
